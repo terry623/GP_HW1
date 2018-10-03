b449c4b859b5bf6ede49cb3c45bed5f0cc99044b
terry623 <terry92516@gmail.com>
2018-10-03 14:14:13 +0800

    add police car ( not move )

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        // transform.Translate(Vector3.forward * 1.0f);
+    }
+}
